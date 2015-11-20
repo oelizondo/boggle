407875f019e70af395f144d4d5f89402a6eea625
oscarmarcelo95 <oscarmarcelo95@gmail.com>
2015-11-20 16:03:32 -0600

    Initial Commit

---
+++ boogle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  boogle
+//
+//  Created by Oscar Elizondo on 11/20/15.
+//  Copyright © 2015 Oscar Elizondo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
